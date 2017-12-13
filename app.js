e0bc6447761570c6bbc0bfcb0322fc119f605634
revathykannan <revathy.kannan26@gmail.com>
2017-12-13 12:29:43 +0530

    changes in index.html

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var cors = require('cors');
 var url = require('url');
-const path = require('path');
-const routes = require('./routes');
-const model = require('./model');
+const path = require('path'); //For Production
+const routes = require('./routes'); //For Production
+const model = require('./model'); //For Production
 
-var url1 = "mongodb://revathy:admin123@ds133746.mlab.com:33746/meanemp";
+var url1 = "mongodb://revathy:admin123@ds133746.mlab.com:33746/meanemp"; //For Production
 mongoose.connect(url1)
     .connection
         .on('connected',function(){
@@ -19,8 +19,8 @@ mongoose.connect(url1)
 var app = express();
 var port = 5000 ;
 app.use(bodyParser.urlencoded({extended: true}));
-app.use('/model',model);
-app.use('/routes',routes);
+app.use('/model', model); //For Production
+app.use('/routes', routes); //For Production
 app.all('*',function(req,res,next){
     res.header("Access-Control-Allow-Origin","*");
     res.header("Access-Control-Allow-Headers","X-Requested-With");
@@ -33,8 +33,8 @@ app.use(cors());
 app.use('/api/employees', routes);
 
 
-app.use(express.static(__dirname + '/public'));
-app.listen(process.env.PORT || 5000);
+app.use(express.static(__dirname + '/public')); //For Production
+app.listen(process.env.PORT || 5000); //For Production
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
+    res.sendFile(path.join(__dirname + '/public/index.html')); //For Production
 });
